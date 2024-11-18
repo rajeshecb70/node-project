07ea0054ef0d5966c69341d145be5b701987c7a3
rajesh <rajesh.jangid@one2n.in>
2024-11-18 16:45:00 +0530

    update the CI file with test and lint

--- node-uptime-cpu/eslint.config.mjs
+++ node-uptime-cpu/eslint.config.mjs
@@ -1,12 +1,27 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    // General configuration for all JavaScript files
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.node,   // Enable Node.js globals like require, module, exports
+      },
+    },
+  },
+  pluginJs.configs.recommended,  // Use ESLint recommended JavaScript rules
+
+  {
+    // Jest-specific configuration for test files
+    files: ["**/*.test.js", "**/*.test.jsx"],  // Specify test file patterns
+    languageOptions: {
+      globals: {
+        ...globals.node,   // Ensure Node.js globals are available for test files
+        jest: true,         // Enable Jest globals like describe, it, expect, afterAll
+      },
+    },
+  },
 ];
